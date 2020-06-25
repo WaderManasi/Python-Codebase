f0585cc7c71e8dd52a27e431a00837d1da61af97
Manasi Wader <manasiwader@gmail.com>
2020-06-25 22:59:24 +0530

    Beginner level code

---
+++ forLoop.py
@@ -0,0 +1,18 @@
+#Multiplication table
+
+def multiplicationTable(N):
+    ## in is a membership operator that is true if something is a member of sequence
+    for i in range(1,11,1): 
+        ## i in range(x,y,z) means i goes from x to y-1 and increments z steps in each iteration
+        print(i*N, end=" ") 
+        ## Separating by spaces using end =" "
+
+def main():
+    t=int(input())
+    while(t>0):
+        int n=int(input())
+        multiplicationTable(n)
+        print()
+        t-=1
+if  __name__=='main':
+    main()
