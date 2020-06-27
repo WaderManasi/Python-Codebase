53511cc3feb829db95f8cdb3c22397693f5bded4
Manasi Wader <manasiwader@gmail.com>
2020-06-27 11:38:14 +0530

    Beginner level code

--- inputStrConcat.py
+++ inputStrConcat.py
@@ -2,18 +2,18 @@
 # You need to take the inputs and print the outputs.
 
 def inPutCat():
-    a=str(input());
-    b=str(input());
-    c=str(input());
+    a=str(input())
+    b=str(input())
+    c=str(input())
     print(a,b,c) 
     ## comma is used as it automatically separates the variables by a space. 
     ## + can also be used to concatenate but it would require manual spaces to print the words with spaces between them.
 
 
 def main():
-    t=int(input());
+    t=int(input())
     while(t>0):
-        inPutCat();
-        t-=1;
+        inPutCat()
+        t-=1
 if  __name__=='main':
-    main();
+    main()
